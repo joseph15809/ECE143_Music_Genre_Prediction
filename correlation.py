f118c79b594432d016497afc35bde953584137e2
rachelhandran <38197877+rachelhandran@users.noreply.github.com>
2025-11-16 22:29:02 -0800

    Clean up comments on correlation.py

--- correlation.py
+++ correlation.py
@@ -6,32 +6,29 @@
 # Do certain music genres rise and fall together over time?
 #   aka, when pop rises, does R&B also rise? when rock falls, does metal also fall?
 
-import pandas as pd
-
-
-# 1. Load Dataset: Billboard + genres (namisha)
-df = pd.read_csv("billboard_fake.csv")
-# columns: 'year', 'artist', 'song', 'genre', 'rank'
-
-# peek at data
-print("Initial df", df)
-
-# Input CSV
+# Input CSV format
 # date,rank,song,artist,last-week,peak-rank,weeks-on-board,genre
-#2010-12-25,1,Firework,Katy Perry,1,1,8,pop
-#2010-12-25,2,What's My Name?,Rihanna Featuring Drake,3,1,8,R&B
-#2010-12-25,3,Grenade,Bruno Mars,5,3,11,pop
-#2010-12-25,4,Raise Your Glass,P!nk,2,1,10,pop
+# 2010-12-25,1,Firework,Katy Perry,1,1,8,pop
+# 2010-12-25,2,What's My Name?,Rihanna Featuring Drake,3,1,8,R&B
+# 2010-12-25,3,Grenade,Bruno Mars,5,3,11,pop
 
 # Note about input: do not drop duplicates of songs, 
 # we need to acccount for multiple appearances across weeks
 # also, billboard csv will never have duplicate rows by nature
 
-# Need this Format
-# | Week-Yr| Pop  | Rock | Hip-Hop| Country | R&B | ... |
-# |------  |------|------|------|------|------|
-# | 1-2000 | 0.34 | 0.28 | 0.15 | 0.12 | 0.11 |
-# | 3-2001 | 0.36 | 0.24 | 0.18 | 0.11 | 0.09 |
+# Need to make a table in this format
+# week-yr,pop,rock,hip-hop,country,r&b
+# 1-2000,0.34,0.28,0.15,0.12,0.11
+# 3-2001,0.36,0.24,0.18,0.11,0.09
+
+import pandas as pd
+
+# 1. Load Dataset: Billboard + genres (namisha)
+# Ideal input columns: date,rank,song,artist,last-week,peak-rank,weeks-on-board,genre
+df = pd.read_csv("billboard_fake.csv")
+
+# peek at data
+print("\nInitial dataframe\n", df)
 
 # 2. Parse Dates, add 3 Columns year, week, week-yr
 # Parse date as datetime (if not already)
@@ -51,13 +48,9 @@ df['week-yr'] = df['week'].astype(str) + '-' + df['year'].astype(str)
 # Remove rows missing genres or years 
 df = df.dropna(subset=['genre','year'])
 # Normalization of genres names if needed here (rnb = r&b = R&B) TODO
-print("\nAfter cleaning, sample rows:")
-print(df)
+print("\nAfter cleaning, sample rows:",df)
 
 # 4. Build new df: proportion table
-# week-yr,pop,rock,hip-hop,country,r&b
-# 1-2000,0.34,0.28,0.15,0.12,0.11
-# 3-2001,0.36,0.24,0.18,0.11,0.09
 # Group by week-yr and genre, count how many songs in each week per genre
 # essentially asks: how many pop songs were on the chart during this week? how about rap? how aout pop in the next week? etc
 genre_counts = df.groupby(['week-yr','genre']).size().reset_index(name='count')
@@ -81,11 +74,16 @@ genre_counts['proportion'] = genre_counts['count'] / genre_counts['total-in-week
 proportion_table = genre_counts.pivot(index='week-yr', columns='genre', values='proportion')
 print("\nProportion table\n",proportion_table)
 
-# Save cleaned files
-proportion_table.to_csv('genre_proportions_by_weekyr.csv')
-print("\nSaved to genre_proportions_by_weekyr.csv")
+# Save proportion df to file
+proportion_table.to_csv('genre_proportions_by_weekyr_fake.csv')
+print("\nSaved to genre_proportions_by_weekyr_fake.csv")
 #df.to_csv('billboard_cleaned_with_weekyr.csv')
 
 # 5. Correlation Analysis: Pearson/Linear
 correlation = proportion_table.corr(method='pearson') # pearson is linear correlation TODO 
-print(correlation)
+print("\nCorrelation dataframe\n",correlation)
+# Save correlation dataframe to file
+correlation.to_csv('correlation_fake.csv')
+
+# 6. TODO Next Step: Visualize correlation data
+# TODO Replace all fake data with real data
